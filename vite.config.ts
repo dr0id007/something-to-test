a9c5672cabe02f559371e866a0fd7319c0254361
dr0id <nullvoid.nv@gmail.com>
2024-02-14 03:04:45 +0530

    repo name update in config

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({ base: '/<repo>/', plugins: [react()] })
+export default defineConfig({ base: '/something-to-test/', plugins: [react()] })
