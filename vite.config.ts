5f67c268e75fa85ee3cab5700ada42f432cd0431
dr0id <nullvoid.nv@gmail.com>
2024-02-14 03:09:05 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({ base: '/something-to-test/', plugins: [react()] })
+export default defineConfig({
+  base: '/something-to-test/',
+  build: { outDir: 'dist' },
+  plugins: [react()],
+})
