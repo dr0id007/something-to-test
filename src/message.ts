132e6f1889108eee98b2747d41ce68caec76eb5a
dr0id <nullvoid.nv@gmail.com>
2024-02-15 00:52:58 +0530

    minor change

--- src/message.ts
+++ src/message.ts
@@ -1,6 +1,6 @@
 export const message = [
   `True Love never ends and you made me fall in love with you..`,
-  `with your cute si smile, tera sona mukhda, teri aankhein, teri baate, teri aadayein ufff and the way you dance babe sizzling hot dnacer hai tu..`,
+  `with your cute si smile, tera sona mukhda, teri aankhein, teri baate, teri aadayein ufff and the way you dance babe sizzling hot dancer hai tu..`,
   `har ek cheez achi lagti hai teri and most importantly your innocence..`,
   `Khushwarya Thanks for being part of my life and me realize what True love actually is and you are the most important part of my life. `,
   `Aise hi rhio tu, Stay same.`,
